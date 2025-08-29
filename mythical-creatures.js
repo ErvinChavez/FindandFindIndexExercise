31d81b9bcf22e585f293d0832f269be8c9660fca
ErvinChavez <ervinchavez31@gmail.com>
2025-08-29 17:20:05 -0400

    my attempt to the find and findIndex exercise

--- mythical-creatures.js
+++ mythical-creatures.js
@@ -5,3 +5,55 @@ const mythicalCreatures = [
   { name: "Griffin", type: "Air", lastSeen: "Highwind Mountains" },
   { name: "Kraken", type: "Water", lastSeen: "Abyssal Depths" },
 ];
+
+/* Tasks:
+- Use the **`find`** method to locate the first creature of the "Water" type and log its name to the console.
+- Use the **`findIndex`** method to locate the index of the "Griffin" in the mythical creatures array and log it to the console.
+- Use the **`find`** method to locate the first creature last seen in "Enchanted Forest". 
+*/
+
+/* My Attempt..........*/
+
+//Task 1:
+
+const waterTypes = mythicalCreatures.find(function (waterCreature) {
+  if (waterCreature.type === "Water") {
+    return waterCreature.name;
+  }
+});
+
+//Task 2:
+const whereGriffin = mythicalCreatures.findIndex(function (griffinLocation) {
+  if (griffinLocation.name === "Griffin") {
+    return griffinLocation;
+  }
+});
+
+//Task 3:
+
+const enchantedForest = mythicalCreatures.find(function (lastSeen) {
+  if (lastSeen.lastSeen === "Enchanted Forest") {
+    return lastSeen.name;
+  }
+});
+/* Solution...........*/
+
+//Task 1:
+const firstWaterCreature = mythicalCreatures.find(function (creature) {
+  return creature.type === "Water";
+});
+console.log(firstWaterCreature.name);
+
+//Task 2
+const griffinIndex = mythicalCreatures.findIndex(function (creature) {
+  return creature.name === "Griffin";
+});
+console.log(griffinIndex);
+
+//Task 3
+const enchantedForestCreature = mythicalCreatures.find(function (creature) {
+  return creature.lastSeen === "Enchanted Forest";
+});
+console.log(enchantedForestCreature.name);
+
+//Notes: I didn't need the if statements, could of been simpler and needed to console.log them too.
